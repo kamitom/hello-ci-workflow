0d067fbcc82a04b22f4e96007e06d1bf01da59f2
Tom Chen <tom@itri.org.tw>
2019-03-23 09:53:11 +0800

    fix: test test.js for fail test

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ var assert = require("assert")
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(0));
+      // assert.equal(-1, [1, 2, 3].indexOf(0)); // 可通過測試
+      assert.equal(-1, [1, 2, 3].indexOf(1));  // 測試失敗
     })
   })
 })
