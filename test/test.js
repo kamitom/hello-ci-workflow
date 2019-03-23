2b0eff55c4eefe7727293e9e0346bfbe6b3c4949
Tom Chen <tom@itri.org.tw>
2019-03-23 11:39:55 +0800

    fix: fix a error test case

--- test/test.js
+++ test/test.js
@@ -6,7 +6,8 @@ describe('Array', function () {
     it('should return -1 when the value is not present', function () {
       // assert.equal(-1, [1, 2, 3].indexOf(0)); // 可通過測試
       // assert.equal(-1, [1, 2, 3].indexOf(1));  // 測試失敗
-      assert.equal(3, [1, 2, 3].indexOf(5)); // 加入錯誤的測試
+      // assert.equal(3, [1, 2, 3].indexOf(5)); // 加入錯誤的測試
+      assert.equal(-1, [1, 2, 3].indexOf(5)); // 加入 正確的測試
 
     })
   })
